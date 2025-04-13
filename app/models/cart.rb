b58cf2aa5e126321a29b127603650dedd9b5e0ff
Aleh Haurylenia <krabique48@gmail.com>
2025-04-13 04:26:48 +0200

    Add empty cart page

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,8 @@ class Cart < ApplicationRecord
   has_many :products, through: :carts_products
 
   accepts_nested_attributes_for :carts_products
+
+  def empty_cart
+    carts_products.destroy_all
+  end
 end
